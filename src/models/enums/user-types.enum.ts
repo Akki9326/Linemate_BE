561a02debcc359ffa287fe87d71c0b088f215199
nikunj71 <nikunj.dhaduk7@gmail.com>
2024-07-17 17:16:15 +0530

    remove userTypeDefaultRoles const and make method for get role based on userType

--- src/models/enums/user-types.enum.ts
+++ src/models/enums/user-types.enum.ts
@@ -4,3 +4,14 @@ export enum UserType {
 	SupportUser = 'Support User',
 	User = 'User',
 }
+
+export const getPermissionGroup = (userType: UserType): string => {
+	switch (userType) {
+		case UserType.CompanyAdmin:
+			return 'company_admin_role';
+		case UserType.SupportUser:
+			return 'support_user_role';
+		case UserType.User:
+			return 'user_role';
+	}
+};
--- src/utils/helpers/default.role.helper.ts
+++ src/utils/helpers/default.role.helper.ts
@@ -1,26 +1,22 @@
 import DB from '@/databases';
 import { RoleType } from '@/models/enums/role.enum';
-import { UserType } from '@/models/enums/user-types.enum';
-
-const userTypeDefaultRoles = {
-	'Company Admin': 'company_admin_role',
-	'Support User': 'support_user_role',
-	User: 'user_role',
-};
+import { getPermissionGroup, UserType } from '@/models/enums/user-types.enum';
 
 export const findDefaultRole = (userType: UserType) => {
-	return userTypeDefaultRoles[userType];
+	return getPermissionGroup(userType);
 };
 export const insertDefaultRoles = async (tenantId: number, createdBy: number) => {
-	const rolesToInsert = Object.values(userTypeDefaultRoles).map(roleName => ({
-		name: roleName,
-		description: '',
-		type: RoleType.Standard,
-		permissionsIds: [],
-		userIds: [],
-		tenantId,
-		createdBy: createdBy,
-	}));
+	const rolesToInsert = Object.values(UserType)
+		.map(roleName => ({
+			name: getPermissionGroup(roleName),
+			description: '',
+			type: RoleType.Standard,
+			permissionsIds: [],
+			userIds: [],
+			tenantId,
+			createdBy,
+		}))
+		.filter(role => role.name);
 	const response = await DB.Roles.bulkCreate(rolesToInsert);
 	return response;
 };
