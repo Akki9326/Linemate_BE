d855f8e5a1ec15082e05112ede32e633eb07791b
pathik95 <pathik.s.patel@gmail.com>
2024-11-20 17:46:37 +0530

    Fixed issue with DB schema permissions

--- db/sql/17308688299992.AlterPermissions.sql
+++ db/sql/17308688299992.AlterPermissions.sql
@@ -1,6 +1,13 @@
+DO $$
+DECLARE
+    campaigns_id INTEGER;
+    cohort_id INTEGER;
+    campaign_master_id INTEGER;
+    cohort_master_id INTEGER;
+BEGIN
 
-SELECT id INTO campaigns_id3 FROM permissions WHERE name = 'Campaigns';
-SELECT id INTO cohort_id3 FROM permissions WHERE name = 'Cohort';
+SELECT id INTO campaigns_id FROM permissions WHERE name = 'Campaigns';
+SELECT id INTO cohort_id FROM permissions WHERE name = 'Cohort';
 
 
 INSERT INTO permissions (name, type, description, "createdBy")
@@ -14,5 +21,7 @@ SELECT id INTO campaign_master_id FROM permissions WHERE name = 'Manage Campaign
 SELECT id INTO cohort_master_id FROM permissions WHERE name = 'Manage Cohort';
 
 
-update permissions set "parentId" = campaign_master_id where id = campaigns_id3;
-update permissions set "parentId" = cohort_master_id where id = cohort_id3;
+update permissions set "parentId" = campaign_master_id where id = campaigns_id;
+update permissions set "parentId" = cohort_master_id where id = cohort_id;
+
+END $$;
